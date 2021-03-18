2214bac99e23b12c53c340254c63ae4864ee9312
Mehul Jariwala <yashcomputers@Mehul-MacBook-Pro.local>
2021-03-18 22:05:58 +0530

    fix: images

--- src/App.js
+++ src/App.js
@@ -4,14 +4,8 @@ import { getShareImage } from "./helper";
 
 export default function App() {
   const handleImage = async () => {
-    const options = {
-      imageQuality: 1,
-      imageType: "image/jpeg",
-      compressQuality: 0.02,
-      finalSize: 4194304, //4mb Size
-    };
     const selector = document.getElementById("test-demo");
-    let blobURL = await getShareImage(selector, options);
+    let blobURL = await getShareImage(selector);
     console.log(blobURL);
   };
 
@@ -28,8 +22,81 @@ export default function App() {
           <h2>Take a screenshot please !</h2>
           <img style={{ width: "100%" }} src={testImage} alt="i" />
           <img style={{ width: "100%" }} src={testImage} alt="i" />
+          <img style={{ width: "100%" }} src={testImage} alt="i" />
+
+          <img style={{ width: "100%" }} src={testImage} alt="i" />
         </div>
       </div>
     </div>
   );
 }
+
+// import { toJpeg } from "html-to-image";
+
+// /* This is another way to do it, also in C.
+//  ** It is easier to do in editors that do not automatically indent the second
+//  ** through last lines of the comment one space from the first.
+//  ** It is also used in Holub's book, in rule 31.
+//  */
+
+// const getOptions = (options) => {
+//   const defaultOptions = {
+//     imageQuality: options.imageQuality || 1,
+//     compressQuality: options.compressQuality || 0.02,
+//     finalSize: options.finalSize || 4194304,
+//     imageType: options.imageType || "image/png",
+//     pixelRatio: options.pixelRatio || 1,
+//   };
+//   return defaultOptions;
+// };
+
+// const JPEG_OPTIONS = { pixelRatio: 1 };
+
+// const getShareImage = async (selector, options) => {
+//   let { imageQuality, compressQuality, finalSize, imageType } = getOptions(
+//     options
+//   );
+//   const libraryOptions = {
+//     quality: imageQuality,
+//     pixelRatio: JPEG_OPTIONS.pixelRatio,
+//   };
+//   try {
+//     return new Promise((resolve) => {
+//       toJpeg(selector, {quality,pixelRatio})
+//         .then((dataUrl) => {
+//           getBlobURL(dataUrl, imageType).then((blob) => {
+//             console.log("blob", blob, imageQuality, blob.size);
+//             if (blob.size > finalSize) {
+//               imageQuality = imageQuality - compressQuality;
+//               getShareImage(selector, {
+//                 ...options,
+//                 imageQuality: imageQuality,
+//               });
+//             } else {
+//               resolve(URL.createObjectURL(blob));
+//             }
+//           });
+//         })
+//         .catch((err) => {
+//           console.log("ERROR", err);
+//         });
+//     });
+//   } catch (err) {
+//     console.log("ERROR", err);
+//   }
+// };
+
+// const getBlobURL = (dataUrl, imageType) => {
+//   return new Promise((resolve) => {
+//     const binaryString = window.atob(dataUrl.split(",")[1]);
+//     const len = binaryString.length;
+//     const binaryArray = new Uint8Array(len);
+
+//     for (let i = 0; i < len; i += 1) {
+//       binaryArray[i] = binaryString.charCodeAt(i);
+//     }
+//     resolve(new Blob([binaryArray], { type: imageType }));
+//   });
+// };
+
+// export { getTest };
--- src/helper.js
+++ src/helper.js
@@ -1,8 +1,30 @@
 import { toJpeg } from "html-to-image";
 
-const getBlobURL = (url, imageType) => {
+/* This is another way to do it, also in C.
+ ** It is easier to do in editors that do not automatically indent the second
+ ** through last lines of the comment one space from the first.
+ ** It is also used in Holub's book, in rule 31.
+ */
+
+const getOptions = (options) => {
+  const defaultOptions = {
+    quality: (options && options.quality) || 1,
+    compressQuality: (options && options.compressQuality) || 0.02,
+    finalSize: (options && options.finalSize) || 4194304,
+    imageType: (options && options.imageType) || "image/png",
+    pixelRatio: (options && options.pixelRatio) || 1,
+  };
+  return defaultOptions;
+};
+
+/**
+ *
+ * @param { This is base64 URL } dataUrl
+ * @param { Type of image png jpeg etc } imageType
+ */
+const getBlobURL = (dataUrl, imageType) => {
   return new Promise((resolve) => {
-    const binaryString = window.atob(url.split(",")[1]);
+    const binaryString = window.atob(dataUrl.split(",")[1]);
     const len = binaryString.length;
     const binaryArray = new Uint8Array(len);
 
@@ -13,27 +35,40 @@ const getBlobURL = (url, imageType) => {
   });
 };
 
-const getShareImage = (selector, options) => {
-  let { imageQuality, compressQuality, finalSize, imageType } = options;
-  const libraryOptions = { quality: imageQuality, pixelRatio: 1 };
-  return new Promise((resolve) => {
-    toJpeg(selector, libraryOptions)
-      .then((dataUrl) => {
-        getBlobURL(dataUrl, imageType).then((blob) => {
-          if (blob.size > finalSize) {
-            imageQuality = imageQuality - compressQuality;
-            getShareImage(selector, { ...options, imageQuality: imageQuality });
-          } else {
-            resolve(URL.createObjectURL(blob));
-          }
-        });
-      })
-      .catch((err) => {
-        console.log("ERROR", err);
-      });
-  }).catch((err) => {
+/**
+ *
+ * @param { DOM Element which u want to capture } selector
+ * @param { list of options where u can customize the picture } options
+ */
+
+const getShareImage = async (selector, options) => {
+  let {
+    quality,
+    compressQuality,
+    finalSize,
+    imageType,
+    pixelRatio,
+  } = getOptions(options);
+  try {
+    return new Promise((resolve) => {
+      toJpeg(selector, { quality, pixelRatio })
+        .then((dataUrl) => {
+          getBlobURL(dataUrl, imageType).then((blob) => {
+            //Decrease the quality of image until each reach limit
+            if (blob.size > finalSize) {
+              quality = quality - compressQuality;
+              const updatedOptions = { ...options, quality: quality };
+              getShareImage(selector, updatedOptions);
+            } else {
+              resolve(URL.createObjectURL(blob));
+            }
+          });
+        })
+        .catch((err) => console.log("ERROR", err));
+    });
+  } catch (err) {
     console.log("ERROR", err);
-  });
+  }
 };
 
 export { getShareImage };
